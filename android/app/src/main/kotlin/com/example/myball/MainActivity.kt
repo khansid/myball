e899ab80a1799d48d81ce6200b46f3e1e3172472
khansid <sidkhan3454@gmail.com>
2022-03-04 18:40:05 +0500

    My ball app
    My first commit

---
+++ android/app/src/main/kotlin/com/example/myball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        home: BallPage(),
+      ),
+  );
+}
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: const Center(child: Text('Ask Me Anything')),
+        ),
+        body: const Ball(),
+      ),
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: (){
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+
+        },
+          child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
+
+
